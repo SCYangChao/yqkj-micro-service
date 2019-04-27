61d2ebb258e8a1c2daefb46fc26df41cc30b312b
yc_xhs <yc_xhs@qqm.com>
2019-04-27 23:37:54 +0800

    [add]zuul 做网关路由

---
+++ micro-service-gateway/src/main/java/com/yqkj/micro/gateway/MicroServiceGateWayApplication.java
@@ -0,0 +1,30 @@
+package com.yqkj.micro.gateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.http.ZuulServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.RibbonRoutingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+/**
+ *
+  * class_name: MicroServiceGateWayApplication
+  * describe: do
+  * @author: yangchao.cool@gmail.com
+  * creat_date: 下午4:41
+  *
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZuulProxy
+public class MicroServiceGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceGateWayApplication.class, args);
+    }
+
+}
